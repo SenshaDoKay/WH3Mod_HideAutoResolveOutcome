953a9692004c0879fceb49a59ffa412949768a54
Kay <kay.sensha-do@pm.me>
2024-01-04 18:20:15 +0100

    Add some comments

--- script/campaign/mod/HideAutoResolveOutcome.lua
+++ script/campaign/mod/HideAutoResolveOutcome.lua
@@ -4,13 +4,17 @@
 -- Credit Ares106 for letting me use his code from the successor: Ares's remove Auto-resolve Outcome
 
 function setOutcomeInvisible()
+    -- gets UI Component by means of the uicomponent tree
     local autoresolve_outcome = find_uicomponent(core:get_ui_root(), "popup_pre_battle", "mid", "battle_deployment",
         "regular_deployment", "list", "autoresolve_outcome")
+
+    -- if it is actually an uicomponent we set it invisible with the provided CA lua method
     if is_uicomponent(autoresolve_outcome) then
         autoresolve_outcome:SetVisible(false)
     end
 end
 
+-- Listener fires every time a Pre Battle popup renders and runs above function
 core:add_listener(
     "PreBattleScreen",
     "PanelOpenedCampaign",
